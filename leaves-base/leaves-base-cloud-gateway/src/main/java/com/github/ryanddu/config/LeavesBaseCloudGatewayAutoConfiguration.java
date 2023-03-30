d8cf2fda3715525c623c9a968901fa3215747262
chenhuainian <chenhuainian@gzhtedu.cn>
2023-03-30 20:57:36 +0800

    新增leaves-base-cloud-api,leaves-base-cloud-biz,leaves-base-cloud-gateway模块，将各模块改成自动配置

---
+++ leaves-base/leaves-base-cloud-gateway/src/main/java/com/github/ryanddu/config/LeavesBaseCloudGatewayAutoConfiguration.java
@@ -0,0 +1,14 @@
+package com.github.ryanddu.config;
+
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * leaves-base-cloud-gateway自动配置类
+ *
+ * @author: ryan
+ * @date: 2023/3/30 16:39
+ **/
+@Configuration
+public class LeavesBaseCloudGatewayAutoConfiguration {
+
+}
